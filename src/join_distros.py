161646e86d9bc84f6037f91052e2f8c2aa8e9623
Guy Sheffer <guysoft@gmail.com>
2022-02-22 13:49:38 +0200

    Switch to v3 format, seprate official and unofficial menues

--- src/join_distros.py
+++ src/join_distros.py
@@ -10,6 +10,24 @@ def get_path_id(path, output_json):
             return i
     return
 
+def make_unofficial_menu(official):
+    unofficial_subitems = []
+    official_subsystem = official["os_list"]
+    official_entry = {"name": "Official",
+                      "description": "The officialy mainained raspberrypi imager repository",
+                      "icon": "icons/cat_raspberry_pi_os.png",
+                      "subitems": official_subsystem}
+    
+    unofficial_entry = {"name": "Unfficial",
+                    "description": "The community mainained raspberrypi imager repository",
+                    "icon": "https://raw.githubusercontent.com/guysoft/pi-imager/qml/icons/cat_unofficialpi.png",
+                    "subitems": unofficial_subitems}
+    
+    os_list = [official_entry, unofficial_entry]
+    
+    output_json = {"imager": official["imager"], "os_list": os_list}
+    return output_json, unofficial_subitems
+
 if __name__ == "__main__":
     import argparse
     
@@ -20,22 +38,25 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    output_json = {}
-    
     # Load base json
     with open(args.base_json) as f:
-        output_json = json.load(f)
+        official = json.load(f)
+    
+    output_json, unofficial_subitems = make_unofficial_menu(official)
     
     # Add all the extra distros
     for file_basename in os.listdir(args.input_folder):
         full_name = os.path.join(args.input_folder, file_basename)
         json_data = None
+        
         with open(full_name) as f:
             json_data = json.load(f)
-            
-        path = json_data["path"][0]
-        for os_list in json_data["os_list"]:
-            output_json["os_list"][get_path_id(path, output_json)]["subitems"].insert(0, os_list)
+          
+        # Disable path handeling for now
+        # path = json_data["path"][0]
+        # for os_list in json_data["os_list"]:
+        #     output_json["os_list"][get_path_id(path, output_json)]["subitems"].insert(0, os_list)
+        unofficial_subitems.append(json_data["os_list"])
     
     with open(args.output_file, "w") as w:
         json.dump(output_json, w)
