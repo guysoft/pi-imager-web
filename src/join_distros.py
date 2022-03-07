13d6e7ea5f4a35cd054d049251dfedfb13149592
Guy Sheffer <guysoft@gmail.com>
2022-03-07 12:33:57 +0200

    Get official images from web and not wget locally

--- src/join_distros.py
+++ src/join_distros.py
@@ -10,13 +10,12 @@ def get_path_id(path, output_json):
             return i
     return
 
-def make_unofficial_menu(official):
+def make_unofficial_menu(official_url, imager_data):
     unofficial_subitems = []
-    official_subsystem = official["os_list"]
     official_entry = {"name": "Official",
                       "description": "The officialy mainained raspberrypi imager repository",
                       "icon": "icons/cat_raspberry_pi_os.png",
-                      "subitems": official_subsystem}
+                      "subitems_url": official_url}
     
     unofficial_entry = {"name": "Unfficial",
                     "description": "The community mainained raspberrypi imager repository",
@@ -25,7 +24,7 @@ def make_unofficial_menu(official):
     
     os_list = [official_entry, unofficial_entry]
     
-    output_json = {"imager": official["imager"], "os_list": os_list}
+    output_json = {"imager": imager_data, "os_list": os_list}
     return output_json, unofficial_subitems
 
 if __name__ == "__main__":
@@ -33,16 +32,18 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Take a folder of pi-imager json files and merge them on to os_list')
     parser.add_argument('input_folder', type=str, help='Input file of json distros')
-    parser.add_argument('base_json', type=str, help='Input file of json distros')
     parser.add_argument('output_file', type=str, help='Ouptut of json to be uploaded and published')
 
     args = parser.parse_args()
     
-    # Load base json
-    with open(args.base_json) as f:
-        official = json.load(f)
-    
-    output_json, unofficial_subitems = make_unofficial_menu(official)
+    # base json
+    imager_data = {
+        "latest_version": "1.7.1",
+        "url": "https://github.com/guysoft/pi-imager"
+            }
+
+    official_url = "https://downloads.raspberrypi.org/os_list_imagingutility_v3.json"
+    output_json, unofficial_subitems = make_unofficial_menu(official_url, imager_data)
     
     # Add all the extra distros
     for file_basename in os.listdir(args.input_folder):
